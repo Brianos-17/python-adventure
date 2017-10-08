fc9f6e5d6faafa603104d983b0a92a8f26505946
Brian <brianos-17@hotmail.com>
2017-10-08 12:18:29 +0100

    Added more actions

--- actions.py
+++ actions.py
@@ -12,3 +12,29 @@ class Action():
 
   def __str__()self:
     return "{}: {}".format(self.hotkey, self.name)
+
+class MoveNorth(Action):
+  def __init__(self):
+    super().__init__(method=Player.move_north, name='Move north', hotkey='w')
+
+class MoveSouth(Action):
+  def __init__(self):
+    super().__init__(method=Player.move_south, name='Move south', hotkey='s')
+
+class MoveEast(Action):
+  def __init__(self):
+    super().__init__(method=Player.move_east, name='Move east', hotkey='a')
+
+class MoveWest(Action):
+  def __init__(self):
+    super().__init__(method=Player.move_west, name='Move west', hotkey='d')
+
+class ViewInventory(Action):
+  """Prints the players inventory"""
+  def __init__(self):
+    super.__init__(method=Player.print inventory, name='View inventory', hotkey='i')
+
+class Attack(Action):
+  def __init__(self):
+    super().__init__(method=Player.attack, name='Attack', hotkey='q', enemy=enemy)
+
